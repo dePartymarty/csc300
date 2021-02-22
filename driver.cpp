71da7cb57895545ca60817f6546609429d6ca54f
dePartymarty <martin.spinell@cuw.edu>
2021-02-21 18:29:20 -0600

    changed stuff

--- driver.cpp
+++ driver.cpp
@@ -1,13 +1,42 @@
+
 #include "Card.hpp"
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+void getContents(string str){
+   int count = 0, i;
+   int n = str.length();
+   
+   for (i = 0; i < n; i++) {
+      
+      if (str[i] == ']') {
+         
+         count--;
+      }
+      
+      else if (str[i] == '[' && count != 0) {
+         cout << str[i];
+         count++;
+      }
+      
+      else if (str[i] != '[') {
+         cout << str[i];
+      }
+      
+    
+
+}
+}
+
+
 
 int main(int argc, char** argv)
 {
-    Card* KingKrush = new Card("King Krush", 9, 8, 8);
-    (*KingKrush).display();
-    Card* ShadSlayer = new Card("Shadowhood Slayer", 1, 2, 1);
-    (*ShadSlayer).display();
-    Card* CrimRunner = new Card("Crimson Sigil Runner", 1, 1, 1);
-    (*CrimRunner).display();
-    Card* Magehunter = new Card("Magehunter", 3, 2, 3);
-    (*Magehunter).display();
+    string str = "[{\"fname\":\"val1\", \"lname\": \"val2\", \"sums\":[1,2,3]},{\"fname\":\"val3\", \"lname\":\"val4\"}]";
+
+    getContents(str);
+    return 0;
 }
